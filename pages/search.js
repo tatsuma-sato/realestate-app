9d1415eba960f6b9ea9608d9db8b157b73a6f0d5
tatsuma-sato <t.tsuma.s@gmail.com>
2022-02-03 01:30:17 -0800

    rename file

--- pages/search.js
+++ pages/search.js
@@ -9,7 +9,7 @@ import Property from "../components/Property";
 import noresult from "../assets/images/noresult.svg";
 import { fetchApi, baseUrl } from "../utils/fetchApi";
 
-const search = ({ properties }) => {
+const Search = ({ properties }) => {
   const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
 
@@ -57,7 +57,7 @@ const search = ({ properties }) => {
   );
 };
 
-export default search;
+export default Search;
 
 export async function getServerSideProps({ query }) {
   const purpose = query.purpose || "for-rent";
